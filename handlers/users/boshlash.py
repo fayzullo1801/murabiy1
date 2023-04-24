dcd08c6ba29d65c975033cd9ee888ae532d957ee
hyjmnd <131165258+fayzullo1801@users.noreply.github.com>
2023-04-24 21:59:34 +0500

    Update boshlash.py

--- handlers/users/boshlash.py
+++ handlers/users/boshlash.py
@@ -19,7 +19,7 @@ async def malumotlar(message: types.Message):
     malumot += f"🎁 Sizga aytib o`tgan kitoblarimizni yutib olishingiz uchun :\n<b>1  Boshlash</b> buyrug`ini yuborsangiz \n<b>2</b>  Sizga botimiz tominidan alohida link ajratib beriladi \n"
     malumot += f"<b>3</b>  Ushbu linkni ko`proq inlonlarga ulashing \n"
     malumot += f"<b>4</b>  Botimizga azo bo`lgan har bir ishtirokchi uchun sizga <b>1 BALL</b> dan qo`shib boriladi\n"
-    malumot += f" Va anglaganingizdek <b>G`OLIBLAR</b> eng ko`p ball to`plagan ishtirokchilar bo`lishadi   shoshiling 20ta kitoblardan biri sizni kutmoqda!!! "
+    malumot += f" Va anglaganingizdek <b>G`OLIBLAR</b> eng ko`p ball to`plagan ishtirokchilar bo`lishadi   shoshiling 20ta kitoblardan biri sizni kutmoqda!!! \n eslatib o`tamiz ushbu tanlov 15 -mayga qadar davom etadi"
     await message.answer(malumot) 
 
 @dp.message_handler(text="✅ boshlash")
@@ -30,5 +30,5 @@ async def boshlash(message: types.Message):
                 
     text += f"Тanlovda ishtirok etish uchun 👇"
     text += f"https://t.me/murabbiy_akademiy_bot?start={message.from_user.id}"
-    photo_id = "AgACAgIAAxkBAAM-ZD55Id6BhosfV_WlN2yUVjK_0CAAAofGMRsPOvlJYRxeCNafgSsBAAMCAAN4AAMvBA"
+    photo_id = "AgACAgIAAxkBAAIBYmRGtKQIlvl1jU69O92OypYDD1QGAALjxjEblwY4SnLC05i66RsBAQADAgADeQADLwQ"
     await message.reply_photo(photo_id, caption=text) 
