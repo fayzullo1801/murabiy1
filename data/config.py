c458bcfa5ae6ece481b8f3143a6e1ca868284fc2
hyjmnd <131165258+fayzullo1801@users.noreply.github.com>
2023-04-24 20:43:52 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,11 +1,15 @@
-from environs import Env
+#from environs import Env
 
 # environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
+#env = Env()
+#env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = ["6127537918:AAHJnRnq4oub0US8AlmYuOjCEBv7enqWZ1Y"]  # Bot toekn
-ADMINS = ["351350395","1994042333"]  # adminlar ro'yxati
-IP = ["lokalhost"]  # Xosting ip manzili
+#BOT_TOKEN = ["6127537918:AAHJnRnq4oub0US8AlmYuOjCEBv7enqWZ1Y"]  # Bot toekn
+#ADMINS = ["351350395","1994042333"]  # adminlar ro'yxati
+#IP = ["lokalhost"]  # Xosting ip manzili
+import os
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
+ADMINS = list(os.environ.get("ADMINS"))
+IP = str(os.environ.get("ip"))
 CHANNEL = ["-1001935637483"]
